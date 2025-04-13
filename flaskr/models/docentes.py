92c55d1f3a7283509f7b4c33842d65e15c5f15fb
eric-castillo05 <ericcastillo005@gmail.com>
2025-04-13 11:53:59 -0600

    feat: create docente

--- flaskr/models/docentes.py
+++ flaskr/models/docentes.py
@@ -3,7 +3,7 @@ from flaskr.utils.db import db
 
 class Docente(db.Model):
     __tablename__ = 'docentes'
-    id_docente = db.Column(db.String(25), primary_key=True)
+    id_docente = db.Column(db.String(36), primary_key=True)
     nombre_completo = db.Column(db.String(50), nullable=False)
     clave_carrera = db.Column(db.String(15), db.ForeignKey('carreras.clave_carrera'), nullable=False)
     email = db.Column(db.String(50), nullable=False)
--- flaskr/routes/DocentesRoutes.py
+++ flaskr/routes/DocentesRoutes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, jsonify
+import uuid
+
+from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
 from flask_jwt_extended import jwt_required
 
+from flaskr.models import Docente
 from flaskr.utils import Config
+from flaskr.utils.db import db
 from flaskr.services.DocenteService import DocenteService
 
 docentes_bp = Blueprint('docentes_bp', __name__)
@@ -15,3 +19,33 @@ docentesService = DocenteService()
 def get_by_clave(clave_carrera):
     docentes = docentesService.get_all(clave_carrera=clave_carrera)
     return jsonify(docentes), 200
+
+@docentes_bp.route('/create_docente', methods=['POST'])
+@cross_origin(origins=Config.ROUTE, supports_credentials=True)
+@jwt_required()
+def create_docente():
+    data = request.get_json()
+
+    required_fields = ['nombre_completo', 'clave_carrera', 'email']
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Faltan campos obligatorios"}), 400
+
+    new_docente = Docente(
+        id_docente=str(uuid.uuid4()),
+        nombre_completo=data['nombre_completo'],
+        clave_carrera=data['clave_carrera'],
+        email=data['email']
+    )
+
+    db.session.add(new_docente)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Docente creado exitosamente",
+        "docente": {
+            "id_docente": new_docente.id_docente,
+            "nombre_completo": new_docente.nombre_completo,
+            "clave_carrera": new_docente.clave_carrera,
+            "email": new_docente.email
+        }
+    }), 201
