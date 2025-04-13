9394790058ff7b87fa4d23cc7faba832710e8263
eric-castillo05 <ericcastillo005@gmail.com>
2025-04-12 19:45:20 -0600

    chore: fix db

--- flaskr/models/aulas.py
+++ flaskr/models/aulas.py
@@ -2,5 +2,9 @@ from flaskr.utils.db import db
 
 class Aula(db.Model):
     __tablename__ = 'aulas'
-    aula_id = db.Column(db.Integer, primary_key=True)
+    aula_id = db.Column(db.Integer)
     edificio_id = db.Column(db.String(3), db.ForeignKey('edificios.numero_edificio'))
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint('aula_id', 'edificio_id'),
+    )
